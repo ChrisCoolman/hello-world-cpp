4d8fffccfce2585a88f6f01286f39c78a74127a0
ChrisCoolman <159795512+ChrisCoolman@users.noreply.github.com>
2024-09-10 09:03:27 -0400

    Add files via upload

---
+++ ChrisConsoleApplication1.cpp
@@ -0,0 +1,20 @@
+#include <iostream> 
+//#include <windows.h> 
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    /*
+    * uncomment for light show
+    while (true)
+    {
+        system("Color FA");
+        system("Color 0A");
+    } */
+    a = 3;
+    b = 2; 
+    c = a + b;
+    cout << "Hello World! C = " << c << endl;
+}
+
---
+++ helloworld.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    a = 5;
+    b = 5;
+    c = a + b;
+    cout << "C = " << c << endl;
+    string name = "Chris Coleman :)";
+    cout << "My name is " << name << endl;1
+}
+
